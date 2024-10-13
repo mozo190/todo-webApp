dd6e95b6d467b1e84ca29c3e5aea909fb4da1ff6
Molnár Zoltán <mozo37@gmail.com>
2024-10-13 14:41:31 +0200

    Refactor switch-case to if-else statements
    Replaced switch-case syntax with if-else statements in main.py to improve compatibility with older Python versions that do not support the match-case structure. This change maintains the logic while ensuring broader usability.

--- todo/main.py
+++ todo/main.py
@@ -14,49 +14,49 @@ while True:
     user_action = input('Enter command "add", "show", "edit", "complete", or "exit":')
     user_action = user_action.strip()
 
-    match user_action:
-        case 'add':
-            todo = input('Enter todo:') + '\n'
+    # match user_action:
+    if 'add' in user_action:
+        todo = input('Enter todo:') + '\n'
 
-            with open('files/todos.txt', 'r') as file:
-                todos = file.readlines()
+        with open('files/todos.txt', 'r') as file:
+            todos = file.readlines()
 
-            todos.append(todo)
+        todos.append(todo)
 
-            write_into_todos_txt_file()
+        write_into_todos_txt_file()
 
-        case 'show':
-            read_todos_txt_file()
+    if 'show' in user_action:
+        read_todos_txt_file()
 
-            new_todos = []
+        new_todos = []
 
-            for item in todos:
-                new_item = item.strip('\n')
-                new_todos.append(new_item)
+        for item in todos:
+            new_item = item.strip('\n')
+            new_todos.append(new_item)
 
-            for index, todo in enumerate(new_todos):
-                print(f"{index + 1}. {todo}")
+        for index, todo in enumerate(new_todos):
+            print(f"{index + 1}. {todo}")
 
-        case 'edit':
-            read_todos_txt_file()
+    if 'edit' in user_action:
+        read_todos_txt_file()
 
-            index = int(input('Enter index:'))
-            todo = input('Enter todo:') + '\n'
-            todos[index - 1] = todo
+        index = int(input('Enter index:'))
+        todo = input('Enter todo:') + '\n'
+        todos[index - 1] = todo
 
-            write_into_todos_txt_file()
+        write_into_todos_txt_file()
 
-        case 'complete':
-            read_todos_txt_file()
+    if 'complete' in user_action:
+        read_todos_txt_file()
 
-            index = int(input('Enter index:'))
-            todo_to_remove = todos[index - 1].strip('\n')
-            todos.pop(index - 1)
+        index = int(input('Enter index:'))
+        todo_to_remove = todos[index - 1].strip('\n')
+        todos.pop(index - 1)
 
-            write_into_todos_txt_file()
+        write_into_todos_txt_file()
 
-            message = f'Todo with index {index}: "{todo_to_remove}" has been completed.'
-            print(message)
+        message = f'Todo with index {index}: "{todo_to_remove}" has been completed.'
+        print(message)
 
-        case 'exit':
-            break
+    if 'exit' in user_action:
+        break
