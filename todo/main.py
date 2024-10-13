55a4e3d14cb450df6fad95b29968aa53491f5fc1
Molnár Zoltán <mozo37@gmail.com>
2024-10-13 12:46:36 +0200

    Add input strip and edit functionality
    Stripped input to remove surrounding whitespace for commands. Added a new 'edit' command that allows users to update a specific todo item by entering its index.

--- todo/main.py
+++ todo/main.py
@@ -2,6 +2,7 @@ todos = []
 
 while True:
     user_action = input('Enter command:')
+    user_action = user_action.strip()
 
     match user_action:
         case 'add':
@@ -9,5 +10,9 @@ while True:
             todos.append(todo)
         case 'show':
             print(todos)
+        case 'edit':
+            index = int(input('Enter index:'))
+            todo = input('Enter todo:')
+            todos[index-1] = todo
         case 'exit':
             break
