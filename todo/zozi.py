79328e1c5cd2777dd8d5fe41ae7e6ad894765302
Molnár Zoltán <mozo37@gmail.com>
2024-10-17 20:16:14 +0200

    practice

---
+++ todo/zozi.py
@@ -0,0 +1,53 @@
+password = '12345678aaaA'
+
+
+def strength(password):
+    if len(password) <= 8:
+        return 'Weak Password'
+
+    if not any(car.isupper() for car in password):
+        return "Weak Password"
+    if not any(car.isdigit() for car in password):
+        return "Weak Password"
+    else:
+        return 'Strong Password'
+
+
+print(strength(password))
+
+
+def foo(arg):
+    avrg = sum(arg) / len(arg)
+    return avrg
+
+
+print(foo([1, 2, 3, 4, 5]))
+
+
+def greet(name):
+    return f'Hi {name}'
+
+
+print(greet('Zozi'))
+
+
+def strings(str1, str2):
+    return str1 + " " + str2
+
+
+print(strings('Zozi', 'Mwale'))
+
+
+def greets(name):
+    name = name.capitalize()
+    return f'Hi {name}'
+
+
+print(greets('zozi'))
+
+
+def speed(distance, time):
+    return distance / time
+
+
+print(speed(5, 300))
