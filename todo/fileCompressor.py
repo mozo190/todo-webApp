3ba123f7373395561b619dd31878dd6257a08dc9
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 15:29:38 +0200

    refreshed sg.FilesBrowse

--- todo/fileCompressor.py
+++ todo/fileCompressor.py
@@ -2,7 +2,7 @@ import FreeSimpleGUI as sg
 
 label1 = sg.Text("Select files to compress")
 input_box1 = sg.Input()
-choose_button1 = sg.FileBrowse("Choose")
+choose_button1 = sg.FilesBrowse("Choose")
 
 label2 = sg.Text("Select destination folder")
 input_box2 = sg.Input()
