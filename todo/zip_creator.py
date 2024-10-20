00dbc289eed8628edc7b78aab8638a6481e94a29
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 15:52:06 +0200

    refreshed make_archive

--- todo/zip_creator.py
+++ todo/zip_creator.py
@@ -6,7 +6,7 @@ def make_archive(filepaths, dest_folder):
     dest_path = pathlib.Path(dest_folder, 'compressed.zip')
     with zipfile.ZipFile(dest_path, 'w') as zipf:
         for file in filepaths:
-            zipf.write(file)
+            zipf.write(file, arcname=pathlib.Path(file).name)
 
 
 if __name__ == '__main__':
