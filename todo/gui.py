f6c90687bb09d5195b61c4526a986f45a0ca7171
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 13:22:58 +0200

    created window to my todo app

---
+++ todo/gui.py
@@ -0,0 +1,12 @@
+import FreeSimpleGUI
+import FreeSimpleGUI as sg
+
+import functions
+
+label = sg.Text("Todo App")
+input_box = sg.InputText(tooltip="Enter todo", size=(40, 1))
+add_button = sg.Button("Add")
+
+window = FreeSimpleGUI.Window("Todo App", layout=[[label], [input_box, add_button]])
+window.read()
+window.close()
