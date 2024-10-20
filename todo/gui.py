8b5dbc25dba7f52a6c90068c7906b8e7231df989
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 14:48:46 +0200

    added while loop and match/case

--- todo/gui.py
+++ todo/gui.py
@@ -1,14 +1,23 @@
-import FreeSimpleGUI
 import FreeSimpleGUI as sg
 
 import functions
 
 label = sg.Text("My To-do App")
-input_box = sg.InputText(tooltip="Enter todo", size=(40, 1))
+input_box = sg.InputText(tooltip="Enter todo", size=(40, 1), key="todo")
 add_button = sg.Button("Add")
 
-window = FreeSimpleGUI.Window("Todo App",
-                              layout=[[label], [input_box, add_button]],
-                              font='Arial 14')
-window.read()
+window = sg.Window("Todo App",
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 14))
+
+while True:
+    event, values = window.read()
+    match event:
+        case "Add":
+            todos = functions.read_todos_txt_file()
+            todos.append(values["todo"] + '\n')
+            functions.write_into_todos_txt_file(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
