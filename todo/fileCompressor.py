10d29b8646ea7f49316408d9d2cbcc50437937d8
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 13:38:14 +0200

    compress_button added

--- todo/fileCompressor.py
+++ todo/fileCompressor.py
@@ -8,8 +8,11 @@ label2 = sg.Text("Select destination folder")
 input_box2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose")
 
+compress_button = sg.Button("Compress")
+
 window = sg.Window("File Compressor", layout=[[label1, input_box1, choose_button1],
-                                              [label2, input_box2, choose_button2]])
+                                              [label2, input_box2, choose_button2],
+                                              [compress_button]])
 
 window.read()
 window.close()
