6bff9788336943e607c9810d63c0da0e2e918b69
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 15:14:09 +0200

    33. line reformated: + \n deleted

--- todo/gui.py
+++ todo/gui.py
@@ -30,7 +30,7 @@ while True:
             new_todo = values["todo"]
 
             todos = functions.read_todos_txt_file()
-            index = todos.index(todo_to_edit + '\n')
+            index = todos.index(todo_to_edit)
             todos[index] = new_todo + '\n'
             functions.write_into_todos_txt_file(todos)
             window["todos"].update(values=functions.read_todos_txt_file())
