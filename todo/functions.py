1e19a159cebe64425e4ecf9e205f84cf6a0cddbd
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 08:19:36 +0200

    use documentum to methods and implement main

--- todo/functions.py
+++ todo/functions.py
@@ -2,11 +2,18 @@ PATH = 'files/todos.txt'
 
 
 def read_todos_txt_file(file_path=PATH):
+    """ Read the todos.txt file and return the content as a list of strings. """
     with open(file_path, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
 def write_into_todos_txt_file(todos, path=PATH):
+    """ Write the todos list into the todos.txt file. """
     with open(path, 'w') as file_local:
         file_local.writelines(todos)
+
+
+if __name__ == '__main__':
+    print("This is a module with functions for the todo app.")
+    print(read_todos_txt_file())
