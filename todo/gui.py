00ee5dccd2ab75e6c78ef67a92168b57fd15edc4
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 15:11:06 +0200

    case "Edit" created

--- todo/gui.py
+++ todo/gui.py
@@ -15,8 +15,9 @@ window = sg.Window("Todo App",
 
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values["todos"])
     match event:
         case "Add":
             todos = functions.read_todos_txt_file()
@@ -24,6 +25,14 @@ while True:
             functions.write_into_todos_txt_file(todos)
             window["todo"].update(value="")
             window["todos"].update(values=functions.read_todos_txt_file())
+        case "Edit":
+            todo_to_edit = values["todos"][0]
+            new_todo = values["todo"]
+
+            todos = functions.read_todos_txt_file()
+            index = todos.index(todo_to_edit + '\n')
+            todos[index] = new_todo + '\n'
+            functions.write_into_todos_txt_file(todos)
         case sg.WIN_CLOSED:
             break
 
