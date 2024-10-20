fdef30994ad4e68c197d9e77fe0d95b74180bc78
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 14:58:56 +0200

    edit_button created

--- todo/gui.py
+++ todo/gui.py
@@ -7,7 +7,7 @@ input_box = sg.InputText(tooltip="Enter todo", size=(40, 1), key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.read_todos_txt_file(), size=(40, 10),
                       key="todos", enable_events=True)
-edit_button
+edit_button = sg.Button("Edit")
 
 window = sg.Window("Todo App",
                    layout=[[label], [input_box, add_button]],
