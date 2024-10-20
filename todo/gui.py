f9b8fb2d57abd9a49de052529e1229272a9a0f05
Molnár Zoltán <mozo37@gmail.com>
2024-10-20 14:36:45 +0200

    font added to window

--- todo/gui.py
+++ todo/gui.py
@@ -7,6 +7,8 @@ label = sg.Text("My To-do App")
 input_box = sg.InputText(tooltip="Enter todo", size=(40, 1))
 add_button = sg.Button("Add")
 
-window = FreeSimpleGUI.Window("Todo App", layout=[[label], [input_box, add_button]])
+window = FreeSimpleGUI.Window("Todo App",
+                              layout=[[label], [input_box, add_button]],
+                              font='Arial 14')
 window.read()
 window.close()
