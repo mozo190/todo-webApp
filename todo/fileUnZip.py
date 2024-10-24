f1f7da7e4f846edaf38b320d1374c4cc9041bf71
Molnár Zoltán <mozo37@gmail.com>
2024-10-24 08:36:30 +0200

    while loop whit event

--- todo/fileUnZip.py
+++ todo/fileUnZip.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import fileUnZip_functions as functions
 
 sg.theme("DarkBlue3")
 
@@ -11,12 +12,26 @@ input_box2 = sg.Input()
 choose_button2 = sg.FolderBrowse("Choose", key="destination")
 
 extract_button = sg.Button("Extract")
-output_label = sg.Text(key="output", text_color="green")
+output_label = sg.Text(key="output", text_color="white")
 
 window = sg.Window("File Extractor",
                    layout=[[label1, input_box1, choose_button1],
                            [label2, input_box2, choose_button2],
                            [extract_button, output_label]])
+while True:
+    event, values = window.read()
+    print(1, event, values)
+    print(2, values)
+    filepaths = values["files"].split(";")
+    print(3, filepaths)
+    folder = values["destination"]
+    if event == sg.WIN_CLOSED:
+        break
+    elif event == "Extract":
+        print("Extracting...")
+        functions.extract_files(filepaths[0], folder)
+        window["output"].update("Files extracted successfully")
+        print("Files extracted successfully")
 
-window.read()
+print("Goodbye!")
 window.close()
