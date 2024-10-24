2a778078e0fa1443875694d32162f7b0a67a150a
Molnár Zoltán <mozo37@gmail.com>
2024-10-24 08:08:21 +0200

    create theme, label1-2, buttons

---
+++ todo/fileUnZip.py
@@ -0,0 +1,16 @@
+import PySimpleGUI as sg
+
+
+sg.theme("DarkBlue3")
+
+label1 = sg.Text("Select files to compress")
+input_box1 = sg.Input()
+choose_button1 = sg.FilesBrowse("Choose", key="files")
+
+label2 = sg.Text("Select destination folder")
+input_box2 = sg.Input()
+choose_button2 = sg.FolderBrowse("Choose", key="destination")
+
+extract_button = sg.Button("Extract")
+output_label = sg.Text(key="output", text_color="green")
+
