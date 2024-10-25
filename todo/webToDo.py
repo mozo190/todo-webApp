87e4c0352ac02b59169496910c8d23e67616ac2b
Molnár Zoltán <mozo37@gmail.com>
2024-10-25 18:30:10 +0200

    create a new web todo App

---
+++ todo/webToDo.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+import functions
+
+todos = functions.read_todos_txt_file()
+# Set the title and subheader of the app
+st.title('Web ToDo App')
+st.subheader('A simple ToDo app to keep track of your tasks')
+st.write('Welcome to the Web ToDo App!')
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label='Add a new task')
