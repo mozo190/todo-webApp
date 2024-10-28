560289ae166a9866f28e6ed6e164afa009f2a7ce
Molnár Zoltán <mozo37@gmail.com>
2024-10-28 07:39:44 +0100

    use webcam

---
+++ todo/webcam.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from PIL import Image
+
+# Start the webcam
+camera_image = st.camera_input('Please take a picture')
+print(camera_image)
+
+# Display the image
+img = Image.open(camera_image)
+
+# Convert the image to grayscale
+gray_img = img.convert('L')
+st.image(gray_img)
