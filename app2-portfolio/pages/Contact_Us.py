bf928d4db909a399e23b037595673036870a1608
Molnár Zoltán <mozo37@gmail.com>
2024-11-23 20:06:15 +0100

    creating email in form

--- app2-portfolio/pages/Contact_Us.py
+++ app2-portfolio/pages/Contact_Us.py
@@ -1,14 +1,20 @@
 import streamlit as st
 
+from send_email import send_email
+
 st.header("Contact Us")
 
 with st.form(key='contact_form'):
     name = st.text_input("Name")
     email = st.text_input("Your Email address")
     message = st.text_area("Your Message")
+
+    message_to_you = f"""
+    Subject: New email from {name} ({email})
+    From: {email}
+    {message}
+    """
     submit = st.form_submit_button("Submit")
 
     if submit:
-        st.write(f"Thank you {name} for your message. We will contact you soon.")
-        st.stop()
-
+        send_email(message_to_you)
--- app2-portfolio/send_email.py
+++ app2-portfolio/send_email.py
@@ -1,24 +1,18 @@
 import smtplib
 import ssl
 
-port = 465  # For SSL
-smtp_server = "smtp.gmail.com"
 
-sender_email = " [email protected]"  # Enter your address
-receiver_email = " [email protected]"  # Enter receiver address
+def send_email(message):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = " [email protected]"  # Enter your address
+    receiver_email = " [email protected]"  # Enter receiver address
+    password = "your_password"  # Enter your password
+    context = ssl.create_default_context()
 
-password = input("Type your password and press enter: ")
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
 
-context = ssl.create_default_context()
 
-message = """
-Subject: Hi there
-
-This message is sent from Python.
-
-Bye!
-"""
-
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+send_email()
