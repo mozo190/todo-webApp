266782703c88a2a39a280a476e2ce4ef25031f42
Molnár Zoltán <mozo37@gmail.com>
2024-11-23 19:13:19 +0100

    add contact form to the Contact Us side

--- app2-portfolio/pages/Contact_Us.py
+++ app2-portfolio/pages/Contact_Us.py
@@ -1,3 +1,14 @@
 import streamlit as st
 
 st.header("Contact Us")
+
+with st.form(key='contact_form'):
+    name = st.text_input("Name")
+    email = st.text_input("Your Email address")
+    message = st.text_area("Message")
+    submit = st.form_submit_button("Submit")
+
+    if submit:
+        st.write(f"Thank you {name} for your message. We will contact you soon.")
+        st.stop()
+
