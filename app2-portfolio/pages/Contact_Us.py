0468b503f8e57456e3da84e6bad3840f2264c8e6
Molnár Zoltán <mozo37@gmail.com>
2024-11-23 20:30:04 +0100

    fix some issues

--- app2-portfolio/pages/Contact_Us.py
+++ app2-portfolio/pages/Contact_Us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL.TiffTags import ASCII
 
 from send_email import send_email
 
@@ -9,12 +10,16 @@ with st.form(key='contact_form'):
     email = st.text_input("Your Email address")
     message = st.text_area("Your Message")
 
-    message_to_you = f"""
+    message_to_you = f"""\
     Subject: New email from {name} ({email})
+    
     From: {email}
     {message}
+    
+    Buy!
     """
     submit = st.form_submit_button("Submit")
 
     if submit:
         send_email(message_to_you)
+        st.success("Your message has been sent!")
--- app2-portfolio/send_email.py
+++ app2-portfolio/send_email.py
@@ -5,14 +5,15 @@ import ssl
 def send_email(message):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    sender_email = " [email protected]"  # Enter your address
-    receiver_email = " [email protected]"  # Enter receiver address
-    password = "your_password"  # Enter your password
+
+    username = " mozo37@"  # Enter your address
+    password = "......"  # Enter your password
+
+    receiver_email = "mozo37@"  # Enter receiver address
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        server.login(username, password)
+        server.sendmail(username, receiver_email, message)
 
 
-send_email()
