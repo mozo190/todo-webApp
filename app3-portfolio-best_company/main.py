599f87e0965caa0ed9f3f71611aa7f7eca935706
Molnár Zoltán <mozo37@gmail.com>
2024-11-23 18:47:38 +0100

    change cols with title()

--- app3-portfolio-best_company/main.py
+++ app3-portfolio-best_company/main.py
@@ -1,5 +1,5 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
 st.set_page_config(page_title="The best company", page_icon="🧊", layout="wide")
 
@@ -16,8 +16,19 @@ col1, empty_col, col2, empty_col2, col3 = st.columns([1, 0.5, 1, 0.5, 1])
 df = pd.read_csv('data.csv', sep=',')
 
 with col1:
-    for i, row in df[:2].iterrows():
-        st.header(row['first name'] + " " + row['last name'])
-        st.write(df['role'])
-        st.image("images/" + df['image'])
+    for i, row in df[:4].iterrows():
+        st.header(row['first name'].title() + " " + row['last name'].title())
+        st.write(row['role'])
+        st.image("images/" + row['image'])
 
+with col2:
+    for i, row in df[4:8].iterrows():
+        st.header(row['first name'].title() + " " + row['last name'].title())
+        st.write(row['role'])
+        st.image("images/" + row['image'])
+
+with col3:
+    for i, row in df[8:].iterrows():
+        st.header(row['first name'].title() + " " + row['last name'].title())
+        st.write(row['role'])
+        st.image("images/" + row['image'])
