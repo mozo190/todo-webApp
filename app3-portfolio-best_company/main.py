ea50d782dffcfd0a77992e644a91ced578981766
Molnár Zoltán <mozo37@gmail.com>
2024-11-23 18:33:08 +0100

    Initial app

--- app3-portfolio-best_company/main.py
+++ app3-portfolio-best_company/main.py
@@ -1,3 +1,16 @@
 import streamlit as st
-import pandas as pd
 
+st.set_page_config(page_title="The best company", page_icon="🧊", layout="wide")
+
+st.title("The best company")
+content = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+"""
+
+st.subheader("Our Team")
+
+col1, empty_col, col2, empty_col2, col3 = st.columns([1, 0.5, 1, 0.5, 1])
+
+with col1:
+    st.header
