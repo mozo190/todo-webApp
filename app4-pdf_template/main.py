f9d4dd3a8e568505530ad08c11b69f834553589e
Molnár Zoltán <mozo37@gmail.com>
2024-11-24 14:42:55 +0100

    draw lines into pages with for loop

--- app4-pdf_template/main.py
+++ app4-pdf_template/main.py
@@ -11,7 +11,10 @@ for i, row in df.iterrows():
     pdf.set_font("Arial", size=24, style='B')
     pdf.set_text_color(100, 100, 100)
     pdf.cell(0, 12, txt=row["Topic"], ln=True, align='L')
-    pdf.line(10, 20, 200, 20)
+
+    # lines with for loop
+    for z in range(20, 280, 10):
+        pdf.line(10, z, 200, z)
 
 
     #set the footer
@@ -28,5 +31,8 @@ for i, row in df.iterrows():
         pdf.set_font("Arial", size=8, style='I')
         pdf.set_text_color(100, 100, 100)
         pdf.cell(0, 10, txt=row["Topic"], align='R')
+
+        for y in range(10, 280, 10):
+            pdf.line(10, y, 200, y)
 
 pdf.output("output.pdf")
