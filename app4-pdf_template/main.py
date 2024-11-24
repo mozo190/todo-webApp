57e549a8b2f3043d6a24fee6175de2a0e28d319c
Molnár Zoltán <mozo37@gmail.com>
2024-11-24 12:26:02 +0100

    insert a for loop, import pandas, set pdf form

---
+++ app4-pdf_template/main.py
@@ -0,0 +1,15 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+df = pd.read_csv("topics.csv")
+
+for i, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font("Arial", size=24, style='B')
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(0, 12, txt=row["Topic"], ln=True, align='L')
+    pdf.line(10, 20, 200, 20)
+
+pdf.output("output.pdf")
