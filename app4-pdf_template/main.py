b218a47a62eefd0253b6511bc7ac3ed3a6d8d6f4
Molnár Zoltán <mozo37@gmail.com>
2024-11-24 14:29:58 +0100

    change footer position

--- app4-pdf_template/main.py
+++ app4-pdf_template/main.py
@@ -13,17 +13,18 @@ for i, row in df.iterrows():
     pdf.cell(0, 12, txt=row["Topic"], ln=True, align='L')
     pdf.line(10, 20, 200, 20)
 
-    pdf.ln(265)
 
     #set the footer
+    pdf.ln(265)
     pdf.set_font("Arial", size=8, style='I')
     pdf.set_text_color(100, 100, 100)
     pdf.cell(0,10,txt=row["Topic"], align='R')
 
     for j in range(row["Pages"]-1):
         pdf.add_page()
-        pdf.ln(278)
+
         # set the footer
+        pdf.ln(275)
         pdf.set_font("Arial", size=8, style='I')
         pdf.set_text_color(100, 100, 100)
         pdf.cell(0, 10, txt=row["Topic"], align='R')
