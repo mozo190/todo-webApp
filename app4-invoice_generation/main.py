5b8ec194797c5e232c7141a2ae5fe68e0e56d18b
Molnár Zoltán <mozo37@gmail.com>
2024-11-25 20:10:40 +0100

    insert a sum

--- app4-invoice_generation/main.py
+++ app4-invoice_generation/main.py
@@ -4,7 +4,6 @@ from fpdf import FPDF
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_excel("invoices/10001-2023.1.18.xlsx", engine='openpyxl')
-print(df.columns)
 
 pdf.add_page()
 pdf.set_font("Arial", size=16, style='B')
@@ -24,9 +23,18 @@ for i, row in df.iterrows():
     pdf.set_font("Times", size=10)
     pdf.cell(25, 10, txt=str(row["product_id"]), border=1, align='L')
     pdf.cell(65, 10, txt=str(row["product_name"]), border=1, align='L')
-    pdf.cell(25, 10, txt=str(row["amount_purchased"]), border=1, align='L')
-    pdf.cell(35, 10, txt=str(row["price_per_unit"]), border=1, align='L')
-    pdf.cell(40, 10, txt=str(row["total_price"]), border=1, align='L')
+    pdf.cell(25, 10, txt=str(row["amount_purchased"]), border=1, align='C')
+    pdf.cell(35, 10, txt=str(row["price_per_unit"]), border=1, align='C')
+    pdf.cell(40, 10, txt=str(row["total_price"]), border=1, align='C')
     pdf.ln()
+
+big_total = df["total_price"].sum()
+
+pdf.set_font("Times", size=10)
+pdf.cell(25, 10, txt="", border=1, align='L')
+pdf.cell(65, 10, txt="", border=1, align='L')
+pdf.cell(25, 10, txt="", border=1, align='L')
+pdf.cell(35, 10, txt="", border=1, align='L')
+pdf.cell(40, 10, txt=str(f"{big_total}"), border=1, align='C')
 
 pdf.output("output.pdf")
