4011293972eff70ae1415011cfa839b8f8d7e2e1
Molnár Zoltán <mozo37@gmail.com>
2024-11-26 18:29:47 +0100

    open different files

--- PythonProject/main.py
+++ PythonProject/main.py
@@ -9,12 +9,18 @@ pdf.set_auto_page_break(auto=False, margin=0)
 for file in glob.glob("files/*.txt"):
     with open(file, "r") as f:
         text = f.read()
+
     filename = Path(file).stem
     pdf.add_page()
+
     pdf.set_font("Arial", size=16, style='B')
     pdf.set_text_color(100, 100, 100)
     pdf.cell(0, 12, txt=f"{filename}", ln=True, align='L')
     pdf.ln()
+
+    pdf.set_font("Arial", size=12)
+    pdf.set_text_color(0, 0, 0)
     pdf.multi_cell(0, 10, txt=text)
-    pdf.output(f"output.pdf")
+
+pdf.output(f"output.pdf")
 
