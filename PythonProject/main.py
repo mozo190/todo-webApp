0a4bbb2a5cc601636a8f645af0e921c76bff6aa7
Molnár Zoltán <mozo37@gmail.com>
2024-11-26 18:27:33 +0100

    initialize the project

---
+++ PythonProject/main.py
@@ -0,0 +1,20 @@
+import glob
+
+from fpdf import FPDF
+from pathlib import Path
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+pdf.set_auto_page_break(auto=False, margin=0)
+
+for file in glob.glob("files/*.txt"):
+    with open(file, "r") as f:
+        text = f.read()
+    filename = Path(file).stem
+    pdf.add_page()
+    pdf.set_font("Arial", size=16, style='B')
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(0, 12, txt=f"{filename}", ln=True, align='L')
+    pdf.ln()
+    pdf.multi_cell(0, 10, txt=text)
+    pdf.output(f"output.pdf")
+
