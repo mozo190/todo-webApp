f062ed7f1b0a4f6b107db5acc2e8edfad3035a5e
Molnár Zoltán <mozo37@gmail.com>
2024-11-28 18:54:10 +0100

    send news email via api

--- app5-daily_news_from_API/main.py
+++ app5-daily_news_from_API/main.py
@@ -1,13 +1,17 @@
 import requests
-from data import NEWS_API_URL, NEWS_API_KEY
+from data import NEWS_API_URL, NEWS_API_KEY, E_MAIL
+from send_email import send_email
 
 api = NEWS_API_KEY
 url = NEWS_API_URL
 
 request = requests.get(url)
 content = request.json()
+
+body = ""
 for i in content['articles']:
-    print(i['title'])
-    print(i['description'])
-    print(i['url'])
-    print()
+    if i['title'] and i['description'] and i['url'] is not None:
+        body += i['title'] + '\n' + i['description'] + '\n' + i['url'] + '\n\n'
+
+body = body.encode('utf-8')
+send_email(body, E_MAIL)
