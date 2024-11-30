cd9636105806685eff49991bfef256f7ee0da9ec
Molnár Zoltán <mozo37@gmail.com>
2024-11-30 19:08:00 +0100

    insert about.html, contact.html, in main.py as well

--- app7-weather_api/main.py
+++ app7-weather_api/main.py
@@ -8,4 +8,12 @@ def home():
     return render_template("home.html")
 
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
 app.run(debug=True)
