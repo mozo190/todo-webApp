bf387bddac39bc834d5d031f253db41aaa87a21b
Molnár Zoltán <mozo37@gmail.com>
2024-11-30 17:27:34 +0100

    load web with NASA picture via API

--- app6-astro-webAPI/main.py
+++ app6-astro-webAPI/main.py
@@ -1,9 +1,26 @@
 import streamlit as st
 import requests
-
+from data import URL
 
 st.set_page_config(page_title="Astronaut picture", page_icon="🧊", layout="wide")
 
-content = """Lorem ipsum dolor sit amet, consectetur adipiscing elit."""
+response = requests.get(URL)
+if response.status_code == 200:
+    content = response.json()
+    image_url = content.get("url")
+    description = content.get("explanation", "No description available")
+else:
+    st.error("Failed to load data")
+    image_url = None
+    description = "No description available"
 
-st.image("images/astronaut.jpg")
+
+# content = """Lorem ipsum dolor sit amet, consectetur adipiscing elit."""
+
+st.title("Astronaut picture")
+if image_url:
+    st.image(image_url, use_container_width=True, caption="Astronaut Image of the Day")
+else:
+    st.error("No image available")
+
+st.write(description)
