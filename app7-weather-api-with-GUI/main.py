e4876d775940ceabf4dd21da4d801be1cd3ee1b8
Molnár Zoltán <mozo37@gmail.com>
2024-12-04 17:54:03 +0100

    insert a plotly line chart

--- app7-weather-api-with-GUI/main.py
+++ app7-weather-api-with-GUI/main.py
@@ -1,3 +1,4 @@
+import plotly.express as px
 import streamlit as st
 
 st.title('Weather Forecast for the Next 5 Days')
@@ -8,3 +9,8 @@ days = st.slider('Forecast Days', min_value=1, max_value=5,
 option = st.selectbox('Select the type of weather data you want to see:',
                       ('Temperature', 'Humidity', 'Wind Speed', 'Pressure'))
 st.subheader(f'{option} for {place} for the next {days} days:')
+
+dates = ['2021-09-01', '2021-09-02', '2021-09-03', '2021-09-04', '2021-09-05']
+temperature = [30, 31, 32, 33, 34]
+fig = px.line(x=dates, y=temperature, title=f'{option} Forecast', labels={'x': 'Date', 'y': option})
+st.plotly_chart(fig)
