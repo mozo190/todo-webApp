465054e5c1fcb3e0102cb2794d28a1bf7f9f0447
Molnár Zoltán <mozo37@gmail.com>
2024-12-06 19:01:28 +0100

    insert get_data method

--- app7-weather-api-with-GUI/backend.py
+++ app7-weather-api-with-GUI/backend.py
@@ -9,8 +9,18 @@ def get_data(place, forecast_days=None, kind=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
     responses = requests.get(url)
     data = responses.json()
-    return data
+    filtered_data = data['list'][:forecast_days * 8]
+    if kind == 'Temperature':
+        return [d['main']['temp'] - 273.15 for d in filtered_data]
+    elif kind == 'Humidity':
+        return [d['main']['humidity'] for d in filtered_data]
+    elif kind == 'Wind Speed':
+        return [d['wind']['speed'] for d in filtered_data]
+    elif kind == 'Pressure':
+        return [d['main']['pressure'] for d in filtered_data]
+    elif kind == "Sky":
+        return [d['weather'][0]['main'] for d in filtered_data]
 
 
 if __name__ == '__main__':
-    print(get_data('Mumbai', 5, 'Temperature'))
+    print(get_data('Mumbai', 3, 'Temperature'))
