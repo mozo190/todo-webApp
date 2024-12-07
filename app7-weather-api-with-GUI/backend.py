52c1989c60f257be798537b569fd5bb46d2c207b
Molnár Zoltán <mozo37@gmail.com>
2024-12-07 17:45:36 +0100

    practice with NLP in jupyter book

--- app7-weather-api-with-GUI/backend.py
+++ app7-weather-api-with-GUI/backend.py
@@ -2,15 +2,14 @@ import os
 
 import requests
 
-API_key = os.environ.get('WEATHER_API_KEY')
-
+# API_key = os.environ.get('WEATHER_API_KEY')
+API_key = 'e295ee1e052f59ce2f1b7c81991f07b9'
 
 def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_key}"
     responses = requests.get(url)
     data = responses.json()
     filtered_data = data['list'][:forecast_days * 8]
-    print(filtered_data)
     return filtered_data
 
 
