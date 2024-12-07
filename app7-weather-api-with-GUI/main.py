f44666abc13fc63c38bcba8fd3e7658d5b5b6264
Molnár Zoltán <mozo37@gmail.com>
2024-12-07 10:06:52 +0100

    insert two options into if statement

--- app7-weather-api-with-GUI/main.py
+++ app7-weather-api-with-GUI/main.py
@@ -8,14 +8,21 @@ place = st.text_input('Enter the name of the place:', 'Mumbai')
 days = st.slider('Forecast Days', min_value=1, max_value=5,
                  help='Select the number of days you want to forecast the weather for.')
 option = st.selectbox('Select the type of weather data you want to see:',
-                      ('Temperature', 'Humidity', 'Wind Speed', 'Pressure'))
+                      ('Temperature', 'Weather', 'Humidity', 'Wind Speed', 'Pressure'))
 st.subheader(f'{option} for {place} for the next {days} days:')
 
+if place:
 
-data = get_data(place, days, option)
+    filtered_data = get_data(place, days)
 
+    if option == 'Temperature':
+        temperatures = [dict["main"]["temp"] - 273.15 for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        # Create a temperature plot
+        fig = px.line(x=d, y=t, title=f'{option} Forecast', labels={'x': 'Date', 'y': 'Temperature (°C)'})
+        st.plotly_chart(fig)
 
-d, t = get_data(days)
-
-fig = px.line(x=d, y=t, title=f'{option} Forecast', labels={'x': 'Date', 'y': 'Temperature (°C)'})
-st.plotly_chart(fig)
+    if option == 'Sky':
+        filtered_data = [dict['weather'][0]["main"] for dict in filtered_data]
+        # Create a weather plot
+        st.image('weather.png')
