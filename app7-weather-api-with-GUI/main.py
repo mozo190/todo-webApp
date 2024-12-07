c2821d818186f0fbdae368be958858cd79526ad5
Molnár Zoltán <mozo37@gmail.com>
2024-12-07 10:38:28 +0100

    if error occurred, try/except method

--- app7-weather-api-with-GUI/main.py
+++ app7-weather-api-with-GUI/main.py
@@ -1,5 +1,6 @@
 import plotly.express as px
 import streamlit as st
+
 from backend import get_data
 
 st.title('Weather Forecast for the Next 5 Days')
@@ -8,21 +9,28 @@ place = st.text_input('Enter the name of the place:', 'Mumbai')
 days = st.slider('Forecast Days', min_value=1, max_value=5,
                  help='Select the number of days you want to forecast the weather for.')
 option = st.selectbox('Select the type of weather data you want to see:',
-                      ('Temperature', 'Weather', 'Humidity', 'Wind Speed', 'Pressure'))
+                      ('Temperature', 'Sky'))
 st.subheader(f'{option} for {place} for the next {days} days:')
 
-if place:
+if place.strip():
+    try:
+        filtered_data = get_data(place, days)
 
-    filtered_data = get_data(place, days)
+        if filtered_data:
 
-    if option == 'Temperature':
-        temperatures = [dict["main"]["temp"] - 273.15 for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
-        # Create a temperature plot
-        fig = px.line(x=d, y=t, title=f'{option} Forecast', labels={'x': 'Date', 'y': 'Temperature (°C)'})
-        st.plotly_chart(fig)
-
-    if option == 'Sky':
-        filtered_data = [dict['weather'][0]["main"] for dict in filtered_data]
-        # Create a weather plot
-        st.image('weather.png')
+            if option == 'Temperature':
+                temperatures = [dict["main"]["temp"] - 273.15 for dict in filtered_data]
+                dates = [dict["dt_txt"] for dict in filtered_data]
+                # Create a temperature plot
+                fig = px.line(x=dates, y=temperatures, title=f'{option} Forecast',
+                              labels={'x': 'Date', 'y': 'Temperature (°C)'})
+                st.plotly_chart(fig)
+            elif option == 'Sky':
+                sky_conditions = [dict['weather'][0]["main"] for dict in filtered_data]
+                st.write(sky_conditions)
+        else:
+            st.error('No data found for the given place.')
+    except Exception as e:
+        st.error(f'An error occurred while fetching the data. {e}')
+else:
+    st.error('Please enter a place name.')
