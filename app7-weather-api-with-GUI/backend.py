3f4059fd91f6f6f69802558d995c0a2da912fafa
Molnár Zoltán <mozo37@gmail.com>
2024-12-07 10:07:41 +0100

    optimized the get_data method

--- app7-weather-api-with-GUI/backend.py
+++ app7-weather-api-with-GUI/backend.py
@@ -10,16 +10,7 @@ def get_data(place, forecast_days=None, kind=None):
     responses = requests.get(url)
     data = responses.json()
     filtered_data = data['list'][:forecast_days * 8]
-    if kind == 'Temperature':
-        return [d['main']['temp'] - 273.15 for d in filtered_data]
-    elif kind == 'Humidity':
-        return [d['main']['humidity'] for d in filtered_data]
-    elif kind == 'Wind Speed':
-        return [d['wind']['speed'] for d in filtered_data]
-    elif kind == 'Pressure':
-        return [d['main']['pressure'] for d in filtered_data]
-    elif kind == "Sky":
-        return [d['weather'][0]['main'] for d in filtered_data]
+    return filtered_data
 
 
 if __name__ == '__main__':
