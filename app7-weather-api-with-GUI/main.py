51e86df9dbae28e110b4972e5b976a618e09a94f
Molnár Zoltán <mozo37@gmail.com>
2024-12-07 11:13:32 +0100

    insert pictures to sky conditions

--- app7-weather-api-with-GUI/main.py
+++ app7-weather-api-with-GUI/main.py
@@ -12,6 +12,13 @@ option = st.selectbox('Select the type of weather data you want to see:',
                       ('Temperature', 'Sky'))
 st.subheader(f'{option} for {place} for the next {days} days:')
 
+images = {
+    "Clear": "images/clear.png",
+    "Clouds": "images/cloud.png",
+    "Rain": "images/rain.png",
+    "Snow": "images/snow.png",
+}
+
 if place.strip():
     try:
         filtered_data = get_data(place, days)
@@ -27,7 +34,14 @@ if place.strip():
                 st.plotly_chart(fig)
             elif option == 'Sky':
                 sky_conditions = [dict['weather'][0]["main"] for dict in filtered_data]
-                st.write(sky_conditions)
+                image_path = [images[sky] for sky in sky_conditions]
+
+                # Display the sky conditions in columns
+                cols = st.columns(5)
+                for i, path in enumerate(image_path):
+                    col = cols[i % 5]
+                    col.image(path, width=100, caption=sky_conditions[i])
+
         else:
             st.error('No data found for the given place.')
     except Exception as e:
