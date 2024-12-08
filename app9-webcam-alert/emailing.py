bd657b7b180f4b1fbc01cae43280dc0902c55646
Molnár Zoltán <mozo37@gmail.com>
2024-12-08 13:30:44 +0100

    create a new email sending method

--- app9-webcam-alert/emailing.py
+++ app9-webcam-alert/emailing.py
@@ -1,2 +1,40 @@
-def send_email():
-    print("Sending email")
+import imghdr
+import smtplib
+from email.message import EmailMessage
+
+PASSWORD = "password"
+EMAIL_ = "mozo37@gmail.com"
+to_email = "mozo37@gmail.com"
+
+
+# def send_email(image=None):
+#     server = smtplib.SMTP("smtp.gmail.com", 587)
+#     server.starttls()
+#     server.login(email, password)
+#     subject = "Security Alert"
+#     body = "Intruder detected"
+#     msg = f"Subject: {subject}\n\n{body}"
+#     server.sendmail(email, to_email, msg)
+#     server.quit()
+
+def send_email(image=None):
+    email_message = EmailMessage()
+    email_message["Subject"] = "Security Alert"
+    email_message.set_content("Intruder detected")
+
+    with open(image, "rb") as f:
+        file_data = f.read()
+        file_name = f.name
+        email_message.add_attachment(file_data,
+                                     maintype="image",
+                                     subtype=imghdr.what(None, file_data),
+                                     filename=file_name)
+        gmail = smtplib.SMTP("smtp.gmail.com", 587)
+        gmail.starttls()
+        gmail.login(EMAIL_, PASSWORD)
+        gmail.sendmail(EMAIL_, to_email, email_message.as_string())
+        gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image="images/1.png")
