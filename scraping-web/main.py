a3617d9805138fb7fb6c8e73e32e6500dd18e325
Molnár Zoltán <mozo37@gmail.com>
2024-12-09 18:13:26 +0100

    create send email method

--- scraping-web/main.py
+++ scraping-web/main.py
@@ -1,5 +1,12 @@
+import ssl
+
 import requests
 import selectorlib
+import smtplib
+
+PASSWORD = "password"
+EMAIL_ = "mozo37@gmail.com"
+to_email = "mozo37@gmail.com"
 
 PAGE_URL = "https://programmer100.pythonanywhere.com/tours/"
 URL_DATA = "/html/body/div[1]/div/h1[3]"
@@ -21,8 +28,17 @@ def extract_data(source_):
     return data
 
 
-def send_email():
-    print("Sending email")
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(EMAIL_, PASSWORD)
+
+        server.sendmail(EMAIL_, to_email, message)
+        print("Email sent successfully")
 
 
 def store(extracted_data):
@@ -30,11 +46,18 @@ def store(extracted_data):
         file.write(extracted_data + '\n')
 
 
+def read_data(extracted_data):
+    with open('data.txt', 'r') as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scraped = scrape(PAGE_URL)
     extracted = extract_data(scraped)
     print(extracted)
-    store(extracted)
+
+    content = read_data(extracted)
     if extracted != "No Upcoming tours":
-        if extracted not in "data.txt":
-            send_email()
+        if extracted not in content:
+            store(extracted)
+            send_email(message="New events was found")
