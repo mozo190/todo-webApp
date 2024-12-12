2238bbe609ca2f3deb4faadcd60550a07be3b30f
Molnár Zoltán <mozo37@gmail.com>
2024-12-12 18:39:32 +0100

    insert hotels.csv, created methods

--- app11-hotel-booking/main.py
+++ app11-hotel-booking/main.py
@@ -1,17 +1,46 @@
-class User:
-    def view_hotels(self):
-        print("Viewing hotels")
+import pandas as pd
+
+df = pd.read_csv('hotels.csv')
 
 
 class Hotel:
+    def __init__(self, id):
+        self.id = id
+        print("Hotel object created")
+
     def book(self):
         print("Booking hotel")
 
-    pass
+    def available_rooms(self):
+        print("Checking available rooms")
 
 
 class Reservation:
     def generate(self):
         print("Generating reservation")
 
-    pass
+    def generate(self):
+        content = f"Reservation for name at {hotel}"
+        print(content)
+        return content
+
+
+class ReservationTicket:
+    def __init__(self, customer_name, hotel):
+        self.customer_name = customer_name
+        self.hotel = hotel
+        print("Reservation ticket created")
+
+    def generate(self):
+        print("Generating reservation ticket")
+
+
+print(df)
+id = input("Enter hotel id: ")
+hotel = Hotel(id)
+
+if hotel.available_rooms():
+    hotel.book()
+    customer_name = input("Enter customer name: ")
+    reservation_ticket = ReservationTicket(customer_name, hotel)
+    print(reservation_ticket.generate())
