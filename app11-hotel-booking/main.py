8dec40daeccde32bd2c2ec1233530e0c55a33ee9
Molnár Zoltán <mozo37@gmail.com>
2024-12-12 18:21:40 +0100

    planning

---
+++ app11-hotel-booking/main.py
@@ -0,0 +1,17 @@
+class User:
+    def view_hotels(self):
+        print("Viewing hotels")
+
+
+class Hotel:
+    def book(self):
+        print("Booking hotel")
+
+    pass
+
+
+class Reservation:
+    def generate(self):
+        print("Generating reservation")
+
+    pass
