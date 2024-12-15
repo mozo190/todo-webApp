32e8496a5b1eacb5bdcc4c9792bdc9dc529d06a6
Molnár Zoltán <mozo37@gmail.com>
2024-12-15 09:50:54 +0100

    avg_speed_label is self.

--- average-speed-calculator/main.py
+++ average-speed-calculator/main.py
@@ -25,8 +25,8 @@ class AvgSpeedCalculator(QWidget):
         calculate_button = QPushButton('Calculate')
         calculate_button.setStyleSheet('background-color: brown; color: white;')
 
-        avg_speed_label = QLabel('Average Speed:')
-        avg_speed_label.setStyleSheet('font-size: 30px; font-weight: bold; color: red;')
+        self.avg_speed_label = QLabel('Average Speed:')
+        self.avg_speed_label.setStyleSheet('font-size: 30px; font-weight: bold; color: red;')
 
         grid.addWidget(distance_label, 0, 0)
         grid.addWidget(self.distance_line_edit, 0, 1)
@@ -34,7 +34,7 @@ class AvgSpeedCalculator(QWidget):
         grid.addWidget(time_label, 1, 0)
         grid.addWidget(self.time_line_edit, 1, 1)
         grid.addWidget(calculate_button, 2, 0, 1, 3)
-        grid.addWidget(avg_speed_label, 3, 0, 1, 3)
+        grid.addWidget(self.avg_speed_label, 3, 0, 1, 3)
 
         calculate_button.clicked.connect(self.calculate_speed)
         self.setLayout(grid)
@@ -47,8 +47,8 @@ class AvgSpeedCalculator(QWidget):
         else:
             speed = distance / time * 0.621371
 
-    # avg_speed_label = self.findChild(QLabel, 'avg_speed_label')
-    self.avg_speed_label.setText(f'Average Speed: {speed}')
+        # avg_speed_label = self.findChild(QLabel, 'avg_speed_label')
+        self.avg_speed_label.setText(f'Average Speed: {speed}')
 
 
 app = QApplication(sys.argv)
