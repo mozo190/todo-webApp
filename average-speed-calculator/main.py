9a18261a65ceb1ad590476def735c4051bf2f293
Molnár Zoltán <mozo37@gmail.com>
2024-12-15 09:42:51 +0100

    create calculate speed method

--- average-speed-calculator/main.py
+++ average-speed-calculator/main.py
@@ -7,21 +7,26 @@ class AvgSpeedCalculator(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('Average Speed Calculator')
-        self.setStyleSheet('font-size: 20px; font-weight: bold; background-color: lightblue;')
+        self.setStyleSheet('font-size: 20px; font-weight: bold; background-color: orange;')
         grid = QGridLayout()
 
         distance_label = QLabel('Distance:')
         self.distance_line_edit = QLineEdit()
+        self.distance_line_edit.setStyleSheet('background-color: white;')
 
         self.metric_combo_box = QComboBox()
         self.metric_combo_box.addItems(['Metric (km)', 'Imperial (miles)'])
+        self.metric_combo_box.setStyleSheet('background-color: white;')
 
         time_label = QLabel('Time (hours)')
         self.time_line_edit = QLineEdit()
+        self.time_line_edit.setStyleSheet('background-color: white;')
 
         calculate_button = QPushButton('Calculate')
+        calculate_button.setStyleSheet('background-color: brown; color: white;')
 
         avg_speed_label = QLabel('Average Speed:')
+        avg_speed_label.setStyleSheet('font-size: 30px; font-weight: bold; color: red;')
 
         grid.addWidget(distance_label, 0, 0)
         grid.addWidget(self.distance_line_edit, 0, 1)
@@ -32,6 +37,18 @@ class AvgSpeedCalculator(QWidget):
         grid.addWidget(avg_speed_label, 3, 0, 1, 3)
 
         self.setLayout(grid)
+
+
+def calculate_speed(self):
+    distance = float(self.distance_line_edit.text())
+    time = float(self.time_line_edit.text())
+    if self.metric_combo_box.currentText() == 'Metric (km)':
+        speed = distance / time
+    else:
+        speed = distance / time * 0.621371
+
+    avg_speed_label = self.findChild(QLabel, 'avg_speed_label')
+    avg_speed_label.setText(f'Average Speed: {speed}')
 
 
 app = QApplication(sys.argv)
