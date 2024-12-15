69cf87afb960bd0f5400cdbc382c225f3d6196e3
Molnár Zoltán <mozo37@gmail.com>
2024-12-15 11:01:26 +0100

    refresh insertDialog method

--- app12-student-management-system/main.py
+++ app12-student-management-system/main.py
@@ -2,7 +2,22 @@ import sqlite3
 import sys
 
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import (QApplication, QMainWindow, QTableWidget, QTableWidgetItem)
+from PyQt6.QtWidgets import (QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QVBoxLayout, QLineEdit)
+
+
+class InsertDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Add Student')
+        self.setFixedSize(300, 200)
+
+        layout = QVBoxLayout()
+
+        self.name_input = QLineEdit()
+        self.name_input.setPlaceholderText('Name:')
+        layout.addWidget(self.name_input)
+
+        self.setLayout(layout)
 
 
 class MainWindow(QMainWindow):
@@ -15,6 +30,7 @@ class MainWindow(QMainWindow):
         help_menu_item = self.menuBar().addMenu('Help')
 
         add_student_action = QAction('Add Student', self)
+        add_student_action.triggered.connect(self.add_student)
         file_menu_item.addAction(add_student_action)
 
         about_action = QAction('About', self)
@@ -23,7 +39,7 @@ class MainWindow(QMainWindow):
         about_action.setMenuRole(QAction.MenuRole.NoRole)
 
         self.table = QTableWidget()
-        self.table.setRowCount(10)
+        self.table.setRowCount(0)
         self.table.setColumnCount(4)
 
         self.table.setHorizontalHeaderLabels(('ID', 'Name', 'Course', 'Mobile'))
@@ -34,10 +50,16 @@ class MainWindow(QMainWindow):
     def load_data(self):
         connection = sqlite3.connect('database.db')
         result = connection.execute('SELECT * FROM students')
+        data = list(result)
+        self.table.setRowCount(len(data))
         for row_number, row_data in enumerate(result):
             for column_number, data in enumerate(row_data):
                 self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
         connection.close()
+
+    def add_student(self):
+        dialog = InsertDialog(self)
+        dialog.exec()
 
 
 app = QApplication(sys.argv)
