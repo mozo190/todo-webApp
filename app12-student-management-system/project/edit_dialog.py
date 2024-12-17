7e52078f66d5dadd480f54f70bc1619f390d4f40
Mozo <mozo37@gmail.com>
2024-12-17 13:51:06 +0100

    Update edit_dialog.py with clicked cell's data

--- app12-student-management-system/project/edit_dialog.py
+++ app12-student-management-system/project/edit_dialog.py
@@ -1,3 +1,32 @@
 class EditDialog(QDialog):
-  def __init__(self):
-    super __init(self):
+  def __init__(self, parent=None, callback=None):
+        super().__init__(parent)
+        self.setWindowTitle('Update Student Data')
+        self.setFixedSize(300, 200)
+
+        self.callback = callback
+
+        layout = QVBoxLayout()
+
+        index = main_window.table.currentRow()
+        student_name = main.window.table.item(index, 1).text()
+    
+        self.name_input = QLineEdit(student_name)
+        layout.addWidget(self.name_input)
+
+        self.course_name = QComboBox()
+        courses = ['Math', 'Science', 'History', 'Computer Science']
+        self.course_name.addItems(courses)
+        layout.addWidget(self.course_name)
+        self.setLayout(layout)
+
+        self.mobile = QLineEdit()
+        self.mobile.setPlaceholderText('Mobile:')
+        layout.addWidget(self.mobile)
+
+        button = QPushButton('Register')
+        button.clicked.connect(self.add_student)
+
+        layout.addWidget(button)
+
+        self.setLayout(layout)
