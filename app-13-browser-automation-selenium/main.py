d75e507ac3d29234982712c44b23978230a72e59
Molnár Zoltán <mozo37@gmail.com>
2024-12-21 19:14:54 +0100

    automation login into a webpage

--- app-13-browser-automation-selenium/main.py
+++ app-13-browser-automation-selenium/main.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+chrome_options = Options()
+chrome_options.add_argument('--disable-search-engine-choice-screen')
 
 service_ = Service('chromedriver-win64/chromedriver.exe')
 driver = webdriver.Chrome(service=service_)
 
 driver.get('https://demoqa.com/login')
 
-input('Press Enter to close the browser...')
+username_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName')))
+password_field = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'password')))
+button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'login')))
 
+username_field.send_keys('zomo')
+password_field.send_keys('Python123@')
+button.click()
+
+input('Press Enter to close the browser...')
 driver.quit()
