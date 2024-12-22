8cee7304f74350f9e44f47b7b2b836ef79283ed4
Molnár Zoltán <mozo37@gmail.com>
2024-12-22 18:25:09 +0100

    formalized template

--- django-form-with-admin/job_application/urls.py
+++ django-form-with-admin/job_application/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     # path('apply/', views.apply, name='apply'),
     # path('success/', views.success, name='success'),
 ]
