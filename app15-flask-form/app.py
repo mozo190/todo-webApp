9fed3b7c21d1f87bed988bc0b859fc8e5fb49f45
Molnár Zoltán <mozo37@gmail.com>
2024-12-22 15:10:30 +0100

    sent an email when the form was submitted

--- app15-flask-form/app.py
+++ app15-flask-form/app.py
@@ -1,12 +1,23 @@
-from flask import Flask, request, render_template, flash
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+from flask import Flask, request, render_template, flash
+from flask_mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+
+app.config['MAIL_USERNAME'] = 'mozo37@gmail.com'
+app.config['MAIL_PASSWORD'] = 'password'
+
 db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
 class Form(db.Model):
@@ -35,6 +46,16 @@ def index():
                     current_position=current_position)
         db.session.add(form)
         db.session.commit()
+
+        msg = Message('Hello',
+                      sender=app.config['MAIL_USERNAME'],
+                      recipients=[email_address],
+                      body=f'Hello {first_name}, your form submitted successfully.\n' 
+                           f'Your current position is {current_position}\n'
+                           f'Your date of birth is {date_input}\n'
+                           f'Thank You!')
+        mail.send(msg)
+
         flash(f'{first_name}, your form submitted successfully')
     return render_template('index.html')
 
