b954a8b1a4f14a172513722fc55c67236f4c3465
Molnár Zoltán <mozo37@gmail.com>
2024-12-22 13:54:14 +0100

    insert database with Form class

--- app15-flask-form/app.py
+++ app15-flask-form/app.py
@@ -1,8 +1,21 @@
-
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email_address = db.Column(db.String(100))
+    date_input = db.Column(db.String(100))
+    current_position = db.Column(db.String(100))
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -15,5 +28,6 @@ def index():
         current_position = request.form['occupation']
         print(first_name, last_name, email_address, date_input, current_position)
     return render_template('index.html')
+
 
 app.run(debug=True, port=5001)
