12bf44edcdf436b7f946fc147730ac572e358a91
Molnár Zoltán <mozo37@gmail.com>
2024-12-22 13:45:06 +0100

    submit button works

--- app15-flask-form/app.py
+++ app15-flask-form/app.py
@@ -1,10 +1,18 @@
 
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    print(request.method)
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email_address = request.form['email']
+        date_input = request.form['date']
+        current_position = request.form['occupation']
+        print(first_name, last_name, email_address, date_input, current_position)
+    return render_template('index.html')
 
 app.run(debug=True, port=5001)
