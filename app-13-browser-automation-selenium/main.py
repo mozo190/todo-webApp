0778993afdf54ea2e6722415079eeabf23937379
Molnár Zoltán <mozo37@gmail.com>
2024-12-22 07:28:06 +0100

    Locate the form fields and submit button, Fill the form fields

--- app-13-browser-automation-selenium/main.py
+++ app-13-browser-automation-selenium/main.py
@@ -21,5 +21,28 @@ username_field.send_keys('zomo')
 password_field.send_keys('Python123@')
 driver.execute_script("arguments[0].click();", login_button)
 
+# Locate the Elements dropdown and Text Box
+elements = (WebDriverWait(driver, 10).
+            until(EC.visibility_of_element_located((By.XPATH,
+                                                    '//*[@id="app"]/div/div/div/div[1]/div/div/div[1]/span/div'))))
+elements.click()
+
+text_box = (WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'item-0'))))
+text_box.click()
+
+# Locate the form fields and submit button
+full_name = (WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userName'))))
+email_address = (WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'userEmail'))))
+current_address = (WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'currentAddress'))))
+permanent_address = (WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, 'permanentAddress'))))
+submit_button = driver.find_element(By.ID, 'submit')
+
+# Fill the form fields
+full_name.send_keys('Zomo')
+email_address.send_keys('zozozo@citromail.com')
+current_address.send_keys('Budapest')
+permanent_address.send_keys('Budapest')
+submit_button.click()
+
 input('Press Enter to close the browser...')
 driver.quit()
