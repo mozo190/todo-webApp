188f595fd3dbd35d01fb73c1542e58d077e2d45d
Molnár Zoltán <mozo37@gmail.com>
2024-12-24 10:29:57 +0100

    insert links with menu detail sites

--- app16-restaurant-menu/restaurant_menu/urls.py
+++ app16-restaurant-menu/restaurant_menu/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MenuList.as_view(), name='index'),
-    path('<int:pk>/', views.MenuDetail.as_view(), name='menu_detail'),
+    path('item/<int:pk>/', views.MenuDetail.as_view(), name='menu_detail'),
 ]
