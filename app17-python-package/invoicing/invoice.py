93d70cdb94bb5849a82f67232f9c731e9cd9cbbc
Molnár Zoltán <mozo37@gmail.com>
2024-12-25 10:16:56 +0100

    before upload to pypi

--- app17-python-package/invoicing/invoice.py
+++ app17-python-package/invoicing/invoice.py
@@ -7,6 +7,19 @@ from fpdf import FPDF
 
 
 def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amount_purchased, price_per_unit, total_price):
+    """
+    Generate a PDF invoice for each Excel invoice in the invoices_path directory.
+    :param invoices_path:
+    :param pdfs_path:
+    :param image_path:
+    :param product_id:
+    :param product_name:
+    :param amount_purchased:
+    :param price_per_unit:
+    :param total_price:
+    :return:
+    """
+
     filepaths = glob.glob(f"{invoices_path}/*.xlsx")
 
     for filepath in filepaths:
@@ -64,5 +77,6 @@ def generate(invoices_path, pdfs_path, image_path, product_id, product_name, amo
         pdf.cell(w=25, h=8, txt=f"PythonHow")
         pdf.image(image_path, w=10)
 
-        os.makedirs(pdfs_path)
-        pdf.output(f"{pdfs_path}/{filename}.pdf")
+        if not os.path.exists(pdfs_path):
+            os.makedirs(pdfs_path)
+            pdf.output(f"{pdfs_path}/{filename}.pdf")
