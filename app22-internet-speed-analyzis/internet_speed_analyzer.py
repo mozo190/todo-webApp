3dd520bfaabc03e49d052ae47989b5fb47677efc
Molnár Zoltán <mozo37@gmail.com>
2024-12-26 14:57:34 +0100

    two endpoints

--- app22-internet-speed-analyzis/internet_speed_analyzer.py
+++ app22-internet-speed-analyzis/internet_speed_analyzer.py
@@ -4,5 +4,16 @@ app = Flask(__name__)
 
 speed_data = []
 
+
+@app.route('/')
+def index():
+    return 'Hello, World!'
+
+
+@app.route('/speedtest')
+def speedtest():
+    return 'Speedtest'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
