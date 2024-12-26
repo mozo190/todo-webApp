6717847ffc996c1cb03084d9317e8b0b3527e01c
Molnár Zoltán <mozo37@gmail.com>
2024-12-26 15:50:42 +0100

    insert measure_speed method, and in main

--- app22-internet-speed-analyzis/internet_speed_analyzer.py
+++ app22-internet-speed-analyzis/internet_speed_analyzer.py
@@ -1,4 +1,8 @@
+import threading
+import time
+
 from flask import Flask, render_template, jsonify
+from speedtest import Speedtest
 
 app = Flask(__name__)
 
@@ -11,7 +15,7 @@ def index():
 
 
 @app.route('/speedtest')
-def speedtest():
+def get_data():
     timestamps = [data['timestamp'] for data in speed_data]
     download_speeds = [data['download_speed'] for data in speed_data]
     upload_speeds = [data['upload_speed'] for data in speed_data]
@@ -22,5 +26,22 @@ def speedtest():
     })
 
 
+def measure_speed():
+    tester = Speedtest()
+    while True:
+        tester.get_best_server()
+        download_speed = tester.download() / 1_000_000  # Convert to Mbps
+        upload_speed = tester.upload() / 1_000_000  # Convert to Mbps
+        speed_data.append({
+            'timestamp': tester.timestamp,
+            'download_speed': download_speed,
+            'upload_speed': upload_speed
+        })
+        if len(speed_data) > 50:  # Keep only the last 50 data points
+            speed_data.pop(0)
+        time.sleep(10)  # Measure speed every 10 seconds
+
+
 if __name__ == '__main__':
+    threading.Thread(target=measure_speed, daemon=True).start()
     app.run(debug=True)
