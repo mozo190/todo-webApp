683d42eae773821d98a0f4f1253b610d3d2789e0
Molnár Zoltán <mozo37@gmail.com>
2024-12-26 11:58:36 +0100

    reformat code

--- app20-open-domain-chatbot/main.py
+++ app20-open-domain-chatbot/main.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 PAGE_ACCESS_TOKEN = "your_facebook_page_access_token"
-VERIFY_TOKEN = "your_facebook_verification_token"
+VERIFY_TOKEN = "my_custom_token1E2/"
 OPENAI_API_KEY = "your_openai_api_key"
 
 openai.api_key = OPENAI_API_KEY
