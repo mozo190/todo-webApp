b2b28cad3e98525472051fd9ddc8a486a7e4ec21
Molnár Zoltán <mozo37@gmail.com>
2024-12-27 14:33:58 +0100

    refresh

--- app2-portfolio/send_email.py
+++ app2-portfolio/send_email.py
@@ -6,10 +6,10 @@ def send_email(message):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
 
-    username = " mozo37@"  # Enter your address
-    password = "......"  # Enter your password
+    username = " mozo37@gmail.com"  # Enter your address
+    password = "EMAIL_PASSWORD"  # Enter your password
 
-    receiver_email = "mozo37@"  # Enter receiver address
+    receiver_email = "mozo37@gmail.com"  # Enter receiver address
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
