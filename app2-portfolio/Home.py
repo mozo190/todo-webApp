d03923fae3c19fe741891a756c510aedda06d89e
Molnár Zoltán <mozo37@gmail.com>
2024-12-27 14:31:58 +0100

    refresh

--- app2-portfolio/Home.py
+++ app2-portfolio/Home.py
@@ -1,25 +1,40 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
 st.set_page_config(page_title="Zoltan Molnar", page_icon="🧊", layout="wide")
 col1, col2 = st.columns(2)
+
+content = """
+    I am a developer and a data scientist. I have a passion for data and I love to work with it.
+    I have experience in Python, Java, SQL, and I am familiar with machine learning and deep learning.
+    I am passionate to learn new things and I am always looking for new challenges.
+    The programing gives me the opportunity to create something new and useful.
+    It is like inventing something new every day.<br><hr style="border: 1px solid lightgrey; margin: 30px 20px;">
+    Below you can find some fo the apps I have created in Python.
+    Feel free to contact me if you have any questions or suggestions.
+    """
+
+# Add spacing between the cards
+card_style = (
+    "border: 0 solid black; border-radius: 10px; padding: 10px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.8);"
+    "margin: 10px;"
+)
 
 with col1:
     st.image('images/mo-zo-480.png')
 
 with col2:
-    st.title("Zoltan Molnar")
-    content = """
-    I am a developer and a data scientist. I have a passion for data and I love to work with it.
-    I have experience in Python, Java, SQL, and I am familiar with machine learning and deep learning.
-    I am passionate to learn new things and I am always looking for new challenges.
-    The programing gives me the opportunity to create something new and useful.
-    It is like inventing something new every day.
-    """
-    st.info(content)
+    with st.container():
+        st.markdown(
+            f"<div style='{card_style}'>"
+            f"<h1 style='text-align: center;'>Zoltan Molnar</h1>"
+            f"<p>{content}</p>",
+            unsafe_allow_html=True,
+        )
 
-st.write("Below you can find some of the apps I have created in Python. Feel free to contact me.")
+st.write("")
 
+# Create columns for the cards
 col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])  # 3 columns
 df = pd.read_csv('data.csv', sep=';')
 
@@ -29,13 +44,19 @@ df['image'] = df['image'].apply(lambda x: f"images/{x}")
 for i, row in df.iterrows():
     if i % 2 == 0:
         with col3:
-            st.header(f" {row['title']}")
-            st.write(row['description'])
-            st.image(row['image'], use_container_width=True)
-            st.write(f"[Source code]({row['url']})")
+            st.markdown(f"<h3>{row['title']}</h3>",unsafe_allow_html=True)
+            st.markdown(f"<p>{row['description']}</p>",unsafe_allow_html=True)
+            st.image(row['image'], use_container_width=True, caption=row['title'])
+            st.markdown(f"<a href='{row['url']}' target='_blank' style='display: inline-block; margin-top: 10px;'>Source code</a>",unsafe_allow_html=True)
+            st.markdown("<hr style='border: 1px solid lightgrey; margin: 30px 20px;'>", unsafe_allow_html=True)
+
     else:
         with col4:
-            st.header(f" {row['title']}")
-            st.write(row['description'])
-            st.image(row['image'], use_container_width=True)
-            st.write(f"[Source code]({row['url']})")
+            st.markdown(f"<h3>{row['title']}</h3>", unsafe_allow_html=True)
+            st.markdown(f"<p>{row['description']}</p>", unsafe_allow_html=True)
+            st.image(row['image'], use_container_width=True, caption=row['title'])
+            st.markdown(
+                f"<a href='{row['url']}' target='_blank' style='display: inline-block; margin-top: 10px;'>Source code</a>",
+                unsafe_allow_html=True)
+            st.markdown("<hr style='border: 1px solid lightgrey; margin: 30px 20px;'>", unsafe_allow_html=True)
+
