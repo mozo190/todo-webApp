659f16915a824e1dfdf4d6e4104be9d41b423111
Molnár Zoltán <mozo37@gmail.com>
2025-01-11 12:51:12 +0100

    Use Streamlit secrets for email password storage
    Replaced the hardcoded email password with Streamlit's secrets management for improved security. This ensures sensitive credentials are no longer exposed directly in the code.

--- app2-portfolio/send_email.py
+++ app2-portfolio/send_email.py
@@ -1,13 +1,15 @@
 import smtplib
 import ssl
 
+import streamlit as st
+
 
 def send_email(message):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
 
     username = " mozo37@gmail.com"  # Enter your address
-    password = "EMAIL_PASSWORD"  # Enter your password
+    password = st.secrets["EMAIL_PASSWORD"]  # Enter your password
 
     receiver_email = "mozo37@gmail.com"  # Enter receiver address
     context = ssl.create_default_context()
@@ -15,5 +17,3 @@ def send_email(message):
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver_email, message)
-
-
