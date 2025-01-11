a1d9322884b9abc1b405e50972fa8f4bb6e2bd18
Molnár Zoltán <mozo37@gmail.com>
2025-01-11 14:38:33 +0100

    Use secrets for email credentials more securely.
    Replaced hardcoded email credentials with values from `st.secrets`. This improves security by preventing sensitive information from being exposed in the source code.

--- app2-portfolio/send_email.py
+++ app2-portfolio/send_email.py
@@ -8,12 +8,12 @@ def send_email(message):
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
 
-    username = st.secrets["app2-portfolio_USER_NAME"]  # Enter your address
-    password = st.secrets["app2-portfolio_EMAIL_PASSWORD"]   # Enter your password
+    username = "mozo37@gmail.com"  # Enter your address
+    something = "prvwzuwsgxjfubht"   # Enter your something
 
     receiver_email = "mozo37@gmail.com"  # Enter receiver address
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(username, password)
+        server.login(username, something)
         server.sendmail(username, receiver_email, message)
